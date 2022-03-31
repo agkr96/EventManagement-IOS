cdab5139fd556778e0978a1da1e69c8605a48130
agkr96 <31437241+agkr96@users.noreply.github.com>
2022-03-30 21:15:24 -0400

    Update String+Extension.swift

--- EventManagement/Extensions/String+Extension.swift
+++ EventManagement/Extensions/String+Extension.swift
@@ -1,1 +1,16 @@
 
+//
+//  UIStoryBoard+Extension.swift
+
+
+import Foundation
+import UIKit
+
+
+
+extension String {
+    
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
