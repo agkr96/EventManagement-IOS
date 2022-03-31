9b81f41614c30a60b4ae074a518ef2920327b717
SriyaReddy-11 <75902849+SriyaReddy-11@users.noreply.github.com>
2022-03-30 21:21:16 -0400

    ui label extension

--- EventManagement/Extensions/UILabel+Extension.swift
+++ EventManagement/Extensions/UILabel+Extension.swift
@@ -1,1 +1,17 @@
-
+import Foundation
+import UIKit
+extension UILabel {
+    func setAttributedString(_ arrStr : [String] , attributes : [[NSAttributedString.Key : Any]]) {
+        let str = self.text!
+        
+        let attributedString = NSMutableAttributedString(string: str, attributes: [NSAttributedString.Key.font: self.font as Any])
+        
+        for index in 0...arrStr.count - 1 {
+            
+            let attr = attributes[index]
+            attributedString.addAttributes(attr, range: (str as NSString).range(of: arrStr[index]))
+        }
+        
+        self.attributedText = attributedString
+    }
+}
