2e9a7629a88fd1625a6fe3ffc346381fecad6457
agkr96 <31437241+agkr96@users.noreply.github.com>
2022-03-30 20:50:40 -0400

    homeNav

--- EventManagement/HomeNav.swift
+++ EventManagement/HomeNav.swift
@@ -1,1 +1,29 @@
 
+//
+//  HomeNav.swift
+//  EventManagement
+
+import UIKit
+
+class HomeNav: UINavigationController {
+
+    var index = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if index == 1 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: EventsVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }else if index == 2 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: MyBookingVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }else if index == 4 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: ContactUSVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+}
