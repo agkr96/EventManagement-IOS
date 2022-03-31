77ef3b844aea6848fcff20e94dabee2b585b61ba
agkr96 <31437241+agkr96@users.noreply.github.com>
2022-03-30 21:13:33 -0400

    usermodel

--- EventManagement/Model/UserModel.swift
+++ EventManagement/Model/UserModel.swift
@@ -1,1 +1,26 @@
 
+//
+//  UserModel.swift
+
+
+import Foundation
+
+
+class UserModel {
+    var id: String!
+    var userName: String!
+    var phone:String!
+    var gender:String!
+    var bDay: String!
+
+    init(id: String,phone:String,userName:String,gender:String,bDay:String){
+        self.id = id
+        self.phone = phone
+        self.bDay = bDay
+        self.gender = gender
+        self.userName = userName
+    }
+    
+    init(){
+    }
+}
