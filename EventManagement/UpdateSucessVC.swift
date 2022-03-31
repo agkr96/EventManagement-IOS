1fc423538bd9580af2d35efaa87b563c0fd1b092
ArvindPanguluri25 <74370692+ArvindPanguluri25@users.noreply.github.com>
2022-03-30 21:05:13 -0400

    Update UpdateSucessVC.swift
    update success

--- EventManagement/UpdateSucessVC.swift
+++ EventManagement/UpdateSucessVC.swift
@@ -1,1 +1,27 @@
+import UIKit
 
+class UpdateSucessVC: UIViewController {
+
+    @IBOutlet weak var btnHome: UIButton!
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    
+    var index: Int = 0
+    
+    @IBAction func btnHomeClick(_ sender: UIButton) {
+        UIApplication.shared.setAdmin(index: 1)
+    }
+    
+    func setUpView(){
+        self.lblTitle.text = index == 0 ? (eUpdateEvent) : (index == 1 ? eDeleteEvent : eUserBlock)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.btnHome.layer.cornerRadius = 5
+        self.setUpView()
+        // Do any additional setup after loading the view.
+    }
+
+}
