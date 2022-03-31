297c9e3ee586134664266d3e7efe8ac7674a01ac
ArvindPanguluri25 <74370692+ArvindPanguluri25@users.noreply.github.com>
2022-03-30 21:01:42 -0400

    Update AdminHomeNav.swift
    home nav

--- EventManagement/AdminHomeNav.swift
+++ EventManagement/AdminHomeNav.swift
@@ -1,1 +1,25 @@
 
+import UIKit
+
+class AdminHomeNav: UINavigationController {
+    
+    var index = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if index == 1 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: MyEventListVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }else if index == 2 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: CreateEventVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }else if index == 3 {
+            if let navigation = UIStoryboard.main.instantiateViewController(withClass: OngoingEventVC.self) {
+                self.pushViewController(navigation, animated: true)
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+}
